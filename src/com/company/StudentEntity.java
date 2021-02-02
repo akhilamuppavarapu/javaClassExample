4db9cfd6bc6728e3c26e4bf5885df06bfa52e710
Akhila Muppavarapu <akhila.muppavarapu2@gmail.com>
2021-02-02 00:20:09 -0500

    base student entity added with default empty constructor and getters/setters

---
+++ src/com/company/StudentEntity.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class StudentEntity {
+
+    private String studentId;
+    private String firstName;
+    private String lastName;
+
+    public StudentEntity() {
+
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
